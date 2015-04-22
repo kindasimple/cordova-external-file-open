8d5f081dd6074ef2b2acd8692a98f9edcf4461d4
Evan White <ewhite@knowledgevault.com>
2015-04-22 13:42:20 -0700

    add package imports that were missing

--- src/android/com/fgomiero/cordova/plugin/ExternalFileUtil.java
+++ src/android/com/fgomiero/cordova/plugin/ExternalFileUtil.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.fgomiero.cordova.plugin;
 
@@ -7,15 +7,17 @@ import java.io.IOException;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaResourceApi;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import android.content.Intent;
 import android.net.Uri;
+import java.io.File;
 
 /**
  * @author Fabio Gomiero
- * 
+ *
  */
 public class ExternalFileUtil extends CordovaPlugin {
 
@@ -38,7 +40,7 @@ public class ExternalFileUtil extends CordovaPlugin {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param url
 	 * @throws IOException
 	 */
@@ -49,43 +51,36 @@ public class ExternalFileUtil extends CordovaPlugin {
 		Uri tmpTarget = Uri.parse(url);
 		final Uri uri = resourceApi.remapUri(tmpTarget.getScheme() != null ? tmpTarget : Uri.fromFile(new File(url)));
 
-		Intent intent = null;
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		//Intent intent = new Intent(Intent.ACTION_SEND);
+		String type = "*/*";
+		//intent = new Intent(Intent.ACTION_VIEW);
 		// Check what kind of file you are trying to open, by comparing the url with extensions.
 		// When the if condition is matched, plugin sets the correct intent (mime) type,
 		// so Android knew what application to use to open the file
 
 		if (url.contains(".doc") || url.contains(".docx")) { // Word document
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setDataAndType(uri, "application/msword");
+			type = "application/msword";
 		} else if (url.contains(".pdf")) { // PDF file
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setDataAndType(uri, "application/pdf");
+			type = "application/pdf";
 		} else if (url.contains(".ppt") || url.contains(".pptx")) { // Powerpoint file
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setDataAndType(uri, "application/vnd.ms-powerpoint");
+			type = "application/vnd.ms-powerpoint";
 		} else if (url.contains(".xls") || url.contains(".xlsx")) { // Excel file
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setDataAndType(uri, "application/vnd.ms-excel");
+			type = "application/vnd.ms-excel";
 		} else if (url.contains(".rtf")) { // RTF file
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setDataAndType(uri, "application/rtf");
+			type = "application/rtf";
 		} else if (url.contains(".wav")) { // WAV audio file
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setDataAndType(uri, "audio/x-wav");
+			type = "audio/x-wav";
 		} else if (url.contains(".gif")) { // GIF file
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setDataAndType(uri, "image/gif");
+			type = "image/gif";
 		} else if (url.contains(".jpg") || url.contains(".jpeg")) { // JPG file
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setDataAndType(uri, "image/jpeg");
+			type = "image/jpeg";
 		} else if (url.contains(".txt")) { // Text file
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setDataAndType(uri, "text/plain");
-		} else if (url.contains(".mpg") || url.contains(".mpeg") 
+			type = "text/plain";
+		} else if (url.contains(".mpg") || url.contains(".mpeg")
 				|| url.contains(".mpe") || url.contains(".mp4")
 				|| url.contains(".avi")) { // Video files
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setDataAndType(uri, "video/*");
+			type = "video/*";
 		}
 
 		// if you want you can also define the intent type for any other file
@@ -94,9 +89,10 @@ public class ExternalFileUtil extends CordovaPlugin {
 		// so you can choose which application to use
 
 		else {
-			intent = new Intent(Intent.ACTION_VIEW);
-			intent.setDataAndType(uri, "*/*");
 		}
+
+		//intent.putExtra(Intent.EXTRA_STREAM, uri);
+		intent.setDataAndType(uri, type);
 
 		this.cordova.getActivity().startActivity(intent);
 	}
